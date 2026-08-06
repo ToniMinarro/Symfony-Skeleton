5a5aca7974d84aa82b759fa13b0fef3b989cf753
Toni Miñarro <antonio_jose91@hotmail.es>
2026-08-06 23:43:07 +0200

    fix: use logical Stimulus AssetMapper path

--- importmap.php
+++ importmap.php
@@ -5,6 +5,12 @@ return [
         'path' => './assets/app.js',
         'entrypoint' => true,
     ],
+    '@symfony/stimulus-bundle' => [
+        'path' => '@symfony/stimulus-bundle/loader.js',
+    ],
+    '@hotwired/stimulus' => [
+        'version' => '3.2.2',
+    ],
     '@hotwired/turbo' => [
         'version' => '8.0.20',
     ],
